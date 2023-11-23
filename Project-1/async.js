ab454bd02ed4dbd7873f78c9c854e638d3016cab
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-11-23 14:17:58 +0530

    Ok

--- Project-1/async.js
+++ Project-1/async.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+console.log('Hello Students');
+
+function callback(){
+    console.log("Hello again from callback");
+
+}
+
+
+setTimeout(callback, 200); //async
+
+console.log("Hello from the final line");
